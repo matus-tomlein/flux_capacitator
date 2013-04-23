0177880d724fe767f2a320f89a78897c7e4c0e32
Matus Tomlein <matus@tomlein.org>
2013-04-24 01:02:53 +0200

    Inject colour styles to diffs

--- web/app/controllers/updates_controller.rb
+++ web/app/controllers/updates_controller.rb
@@ -3,6 +3,7 @@ class UpdatesController < ApplicationController
     @content = 'sasa'
     @content = `python lib/diff.py #{params[:update1_id]} #{params[:update2_id]}` if params.has_key?(:update1_id) && params.has_key?(:update2_id)
 
+    @content.sub! '</head>', '<style type="text/css"><!--ins {background: #bfb} del{background: #fcc} ins,del {text-decoration: none} --></style></head>'
     respond_to do |format|
       format.html { render :text => @content }
     end
