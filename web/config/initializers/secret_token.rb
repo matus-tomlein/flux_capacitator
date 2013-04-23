55a466c0da7d7ee9e7fe7f6841dd6e39433e8c01
Matus Tomlein <matus@tomlein.org>
2013-04-23 02:07:43 +0200

    Add rails web app

---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '81f94671419d50f59c8686c49451a5aa2458cc1d88db0c1327fb3a868f900412dffde88cb9d31a69d66f6c5134a03955a525cb32138016b9c8a51ab4ae0d9c7e'
