bffb01013576ec0ca04b6784b5cb047e25749541
Matus Tomlein <matus@tomlein.org>
2013-06-08 13:00:02 +0200

    Fix a bug in Page, modify .gitignore to ignore tmp folder

--- app/models/page.rb
+++ app/models/page.rb
@@ -22,16 +22,16 @@ class Page < ActiveRecord::Base
     planned_update.save
   end
 
-  def strip_url_and_find(value)
-    find_by_stripped_url(get_stripped_url(value))
+  def self.strip_url_and_find(value)
+    find_by_stripped_url(Page.get_stripped_url(value))
   end
 
   def url=(value)
     write_attribute(:url, value)
-    self.stripped_url = get_stripped_url value
+    self.stripped_url = Page.get_stripped_url value
   end
 
-  def get_stripped_url(value)
+  def self.get_stripped_url(value)
     stripped_url = value.chomp('/').downcase
     stripped_url[0..6] = '' if stripped_url.start_with? "http://"
     stripped_url[0..3] = '' if stripped_url.start_with? "www."
